4803ddd4a71adf86644f957aab641fc000911565
Arif2ankhi <arif2ankhi@gmail.com>
2024-12-04 01:19:17 +0000

    Footer added

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
-// import Footer from './Footer/Footer';
+import Footer from './Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     );
 };
