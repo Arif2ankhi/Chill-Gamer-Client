735a76251facf6e20cf4a31139bc63128849a9c0
Arif2ankhi <arif2ankhi@gmail.com>
2025-03-23 07:23:27 +0000

    Public  repo and live link added in README file

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -2,7 +2,7 @@
 
 import React, { useContext, useRef, useState } from "react";
 import auth from "../../firebase/firebase.config";
-import {  signInWithEmailAndPassword,  } from "firebase/auth";
+import {signInWithEmailAndPassword, } from "firebase/auth";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -56,6 +56,7 @@ const Signin = () => {
 
 const handleGoogleSignIn = () => {
     signInWithGoogle()
+    // signInWithGoogle()
       .then((result) => {
         const user = result.user;
         console.log("Google Sign-In Success:", user);
